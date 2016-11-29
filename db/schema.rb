bf31eb20f39795158f0d4487462f6e802f46529d
YannAries <yannou@protonmail.com>
2016-11-29 15:34:39 +0100

    global commit

--- db/schema.rb
+++ db/schema.rb
@@ -1,7 +1,6 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
-#
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
@@ -27,5 +26,4 @@ ActiveRecord::Schema.define(version: 20161028115733) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
-
 end
